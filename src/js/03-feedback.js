57cd65faa7c35e6190c256c291d818b8ca9286b3
vladislav-kuznetsov666 <Kuznetsovv887@gmail.com>
2023-11-07 21:58:53 +0200

    third task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,45 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('input[name="email"]');
+const messageInput = form.querySelector('textarea[name="message"]');
+
+const saveFormData = () => {
+    const formData = {
+        email: emailInput.value,
+        message: messageInput.value,
+    };
+    localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+};
+
+const loadFormData = () => {
+    const savedData = localStorage.getItem('feedback-form-state');
+    if (savedData) {
+      const formData = JSON.parse(savedData);
+      emailInput.value = formData.email;
+      messageInput.value = formData.message;
+    }
+  };
+
+  emailInput.addEventListener('input', () => {
+    saveFormData();
+  });
+  
+  messageInput.addEventListener('input', () => {
+    saveFormData();
+  });
+
+  loadFormData(); 
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const formData = JSON.parse(localStorage.getItem('feedback-form-state'));
+
+    console.log(formData);
+
+    emailInput.value = '';
+    messageInput.value = '';
+    localStorage.removeItem('feedback-form-state');
+  });
+
+  const throttledSaveFormData = throttle(saveFormData, 500);
