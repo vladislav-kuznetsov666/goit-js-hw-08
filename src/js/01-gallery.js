082d672a3722401430f2df75a81e38e28e503d5b
vladislav-kuznetsov666 <Kuznetsovv887@gmail.com>
2023-11-07 14:55:12 +0200

    first and second tasks

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,40 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
+
+function createGalleryItem(item) {
+    const li = document.createElement('li');
+    li.classList.add('gallery__item');
+  
+    const a = document.createElement('a');
+    a.classList.add('gallery__link');
+    a.href = item.original;
+  
+    const img = document.createElement('img');
+    img.classList.add('gallery__image');
+    img.src = item.preview;
+    img.alt = item.description;
+  
+    a.appendChild(img);
+    li.appendChild(a);
+  
+    return li;
+  }
+  
+  const gallery = document.querySelector('.gallery');
+  
+  gallery.style.listStyle = 'none';
+
+  galleryItems.forEach((item) => {
+    const galleryItem = createGalleryItem(item);
+    gallery.appendChild(galleryItem);
+  });
+  
+  const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
 
 console.log(galleryItems);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,27 @@
+import Vimeo from 'vimeo';
+import throttle from 'lodash.throttle';
+
+const iframe = document.getElementById('vimeo-player');
+const player = new Vimeo.Player(iframe);
+
+player.on('timeupdate', (data) => {
+  const currentTime = data.seconds;
+  localStorage.setItem('videoplayer-current-time', currentTime);
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const savedTime = localStorage.getItem('videoplayer-current-time');
+    if (savedTime) {
+        player.setCurrentTime(savedTime);
+    }
+});
+
+const saveTimeThrottled = throttle((currentTime) => {
+    localStorage.setItem('videoplayer-current-time', currentTime);
+}, 1000);
+
+player.on('timeupdate', (data) => {
+    const currentTime = data.seconds;
+    saveTimeThrottled(currentTime);
+});
 
