588c19febcf9ff1ecd4f224e826c4cfffcf281d5
vladislav-kuznetsov666 <Kuznetsovv887@gmail.com>
2023-11-08 22:08:44 +0200

    fix second task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,13 +1,8 @@
-import Vimeo from 'vimeo';
+import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
 const iframe = document.getElementById('vimeo-player');
-const player = new Vimeo.Player(iframe);
-
-player.on('timeupdate', (data) => {
-  const currentTime = data.seconds;
-  localStorage.setItem('videoplayer-current-time', currentTime);
-});
+const player = new Player(iframe);
 
 document.addEventListener('DOMContentLoaded', () => {
     const savedTime = localStorage.getItem('videoplayer-current-time');
